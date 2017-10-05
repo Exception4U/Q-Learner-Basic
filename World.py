816de2c253a4c40775116254796d0243bb3a258d
Tushar Vaidya <tushar5610@gmail.com>
2017-10-06 00:12:50 +0530

    extended world size

--- World.py
+++ World.py
@@ -6,7 +6,7 @@ triangle_size = 0.1
 cell_score_min = -0.2
 cell_score_max = 0.2
 Width = 100
-(x, y) = (5, 5)
+(x, y) = (10, 10)
 actions = ["up", "down", "left", "right"]
 
 board = Canvas(master, width=x*Width, height=y*Width)
@@ -15,8 +15,8 @@ score = 1
 restart = False
 walk_reward = -0.04
 
-walls = [(1, 1), (1, 2), (2, 1), (2, 2)]
-specials = [(4, 1, "red", -1), (4, 0, "green", 1)]
+walls = [(1, 1), (1, 2), (2, 1), (2, 2), (3,3)]
+specials = [(9, 1, "red", -1), (9, 0, "green", 1)]
 cell_scores = {}
 
 
